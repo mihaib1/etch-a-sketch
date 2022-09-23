93d26ef82bf0f5c06eaef35acf2935e048fc6d3c
Mihai Baiasu <baiasumihai07@yahoo.com>
2022-09-23 10:43:00 +0300

    New version

---
+++ script.js
@@ -0,0 +1,64 @@
+let color ='black';
+let click = true;
+
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
+    let amount = size * size;
+    // aici vom modifica, in loc de 16 vom pune gridSize = input de la user;
+    // 1fr = toate cele 16 celule vor ocupa acelasi procent din grid => nu se va modifica marimea grid-ului
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
+
+    for(let i=0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare)
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if(input >= 2 && input <= 100) {
+        document.querySelector('.error').style.display='none';
+        populateBoard(input);
+    } 
+    else {
+        document.querySelector('.error').style.display='flex';
+    }
+}
+
+function colorSquare() {
+    if(click === true) {
+        if(color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor=color;  // folosim this in loc de square pt a ne referi la obiectul parinte
+        }
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener('click', function(e) {
+    if(e.target.tagName != 'BUTTON') {
+        click = !click;
+        if(click) {
+            document.querySelector('.mode').textContent = 'Mode: Coloring'
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+        }
+    }
+})
